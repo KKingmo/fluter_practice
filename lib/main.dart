c517a8569b63ec007e80fe3e51b379706261b951
Kingmo <55ooccmm@gmail.com>
2024-10-02 00:41:02 +0900

    feat: route 구성해보기

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,37 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Material Design 스타일의 UI 구성 요소를 제공하는 패키지
 
-// main 함수는 Flutter 앱의 진입점으로, runApp 함수로 MyApp 위젯을 실행합니다.
+import 'screen/main_screen.dart'; // MainScreen을 사용하기 위해 import
+import 'screen/splash_screen.dart'; // SplashScreen을 사용하기 위해 import
+
 void main() => runApp(const MyApp());
+// Flutter 앱의 진입점. runApp()은 주어진 위젯을 화면에 띄움
+// const MyApp()는 변하지 않는 값임을 나타내는 상수형 위젯으로, 성능 최적화를 위해 사용
 
-// MyApp은 StatelessWidget으로, 상태를 가지지 않는 위젯입니다.
-// 앱 전체의 구조와 테마를 정의합니다.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  // const 생성자는 위젯이 변하지 않을 때 사용해 성능 향상
+  // StatelessWidget은 상태(변하는 데이터)를 가지지 않는 위젯
 
   @override
   Widget build(BuildContext context) {
+    // build() 메서드는 위젯 트리를 생성
     return MaterialApp(
-      // 앱의 타이틀을 지정합니다.
       title: 'Flutter Demo',
-      // 앱의 테마를 정의합니다. Material 3 디자인을 사용하며, 기본 색상으로 보라색 계열을 사용합니다.
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      // 앱이 시작될 때 보여줄 첫 화면을 지정합니다. 여기서는 MainScreen 위젯을 표시합니다.
-      home: const MainScreen(),
+      // 앱의 이름을 정의. 디버깅 시 사용됨
+      initialRoute: '/',
+      // 초기 경로(route)를 지정. 앱이 처음 시작할 때 보여줄 화면
+
+      routes: {
+        '/': (context) => SplashScreen(),
+        // '/' 경로에서는 SplashScreen 위젯을 반환. 첫 화면
+        '/main': (context) => MainScreen(),
+        // '/main' 경로에서는 MainScreen 위젯을 반환
+      },
     );
   }
 }
 
-// MainScreen은 StatefulWidget으로, 상태를 가지는 위젯입니다.
-// 상태 변화에 따라 화면이 다시 그려질 수 있습니다.
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
-
-  // _MainScreenState 클래스가 상태를 관리합니다.
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-// _MainScreenState는 MainScreen의 상태를 나타내는 클래스입니다.
-class _MainScreenState extends State<MainScreen> {
-  // msg 변수는 화면에 표시될 텍스트를 저장합니다.
-  String msg = '오창모';
-
-  // 위젯이 처음 생성될 때 호출되는 메서드로, 초기 상태 설정을 처리합니다.
-  @override
-  void initState() {
-    super.initState();
-
-    // 3초 후에 msg 값을 '오창모 짱'으로 변경하는 비동기 작업을 실행합니다.
-    Future.delayed(Duration(seconds: 3), () {
-      // setState 함수는 상태를 변경하고 화면을 다시 그리도록 합니다.
-      setState(() {
-        msg = '오창모 짱';
-      });
-    });
-  }
-
-  // 화면에 그려질 UI를 정의하는 메서드입니다.
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // 상단의 앱바를 정의합니다.
-      appBar: AppBar(
-        title: Text('나의 첫 앱'), // 앱바에 표시될 제목입니다.
-      ),
-      // 화면의 본문 부분에 msg 변수를 텍스트로 표시합니다.
-      body: Text(msg),
-    );
-  }
-}
+// StatelessWidget은 변경되지 않는 UI를 렌더링하는데 적합
+// MaterialApp은 Flutter에서 기본적으로 제공하는 앱 구조
+// routes는 특정 경로에 따라 다른 화면을 보여줄 수 있게 하는 중요한 개념
+// 이를 통해 다중 화면 네비게이션을 쉽게 구현
